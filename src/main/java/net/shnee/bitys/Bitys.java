a129b264783e1279f0822ae553d301e9d61f0279
Curtis Wilson <wilson.1339@osu.edu>
2014-09-17 19:14:22 -0400

    First commit.

---
+++ src/main/java/net/shnee/bitys/Bitys.java
@@ -0,0 +1,13 @@
+package net.shnee.bitys;
+
+/**
+ * Hello world!
+ *
+ */
+public class Bitys 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/net/shnee/bitys/BitysTest.java
@@ -0,0 +1,38 @@
+package net.shnee.bitys;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BitysTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BitysTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BitysTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testBitys()
+    {
+        assertTrue( true );
+    }
+}
