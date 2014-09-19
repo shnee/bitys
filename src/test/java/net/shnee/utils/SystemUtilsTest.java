b9d896fd5b93d9dac1ac14f8787d4b65852d5c31
Curtis Wilson <wilson.1339@osu.edu>
2014-09-19 17:58:47 -0400

    - Added loging statements. - Updated methods that changed their names.

--- src/test/java/net/shnee/utils/SystemUtilsTest.java
+++ src/test/java/net/shnee/utils/SystemUtilsTest.java
@@ -1,6 +1,7 @@
 package net.shnee.utils;
 // TODO Move the utlility classes into it's own projects
 
+import org.slf4j.LoggerFactory;
 import java.io.PrintWriter;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -19,6 +20,10 @@ public class SystemUtilsTest {
         /* TODO Add a check to make sure the current operating system is a unix
          *      based one. */
 
+        
+        LoggerFactory.getLogger(SystemUtilsTest.class).
+                      trace("SystemUtilsTest.testRunCommand");
+       
         /* Create a script that will create a text file with the text 'yay' and
          * then output the text in the file. */
         try (PrintWriter writer = new PrintWriter("script.sh", "UTF-8")) {
@@ -38,7 +43,7 @@ public class SystemUtilsTest {
 
         SystemUtils.runCommand("rm script.sh yay.txt");
 
-        assertFalse(FileUtils.fileExist("script.sh"));
-        assertFalse(FileUtils.fileExist("yay.txt"));
+        assertFalse(FileUtils.fileExists("script.sh"));
+        assertFalse(FileUtils.fileExists("yay.txt"));
     }
 }
