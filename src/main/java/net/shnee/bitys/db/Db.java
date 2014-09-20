45eb34a217d715e0267b3f36a2cebfdd1fe8123d
Curtis Wilson <wilson.1339@osu.edu>
2014-09-20 10:35:39 -0400

    - Added database singleton interface.

---
+++ src/main/java/net/shnee/bitys/db/Db.java
@@ -0,0 +1,13 @@
+package net.shnee.bitys.db;
+
+/**
+ * Database interface.
+ */
+public interface Db {
+
+    /**
+     * @return Returns the singleton instance of the database.
+     */
+    public Db getInstance();
+
+}
