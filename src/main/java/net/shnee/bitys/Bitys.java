98238eeeb70352e55af3af5276c36b4828f2dd09
Curtis Wilson <wilson.1339@osu.edu>
2014-09-19 23:24:06 -0400

    - Changed database to mysql.

--- src/main/java/net/shnee/bitys/Bitys.java
+++ src/main/java/net/shnee/bitys/Bitys.java
@@ -1,5 +1,13 @@
 package net.shnee.bitys;
 
+import java.util.Properties;
+import net.shnee.bitys.model.Sport;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +19,35 @@ public class Bitys
 {
     public static void main( String[] args )
     {
-        Logger log = LoggerFactory.getLogger(Bitys.class);
+        Logger log = LoggerFactory.getLogger("Bitys");
         log.info("Word to your mother...");
+        
+        Configuration configuration = new Configuration(); 
+        configuration.configure(); 
+         
+        Properties properties = configuration.getProperties();
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().
+                                                  applySettings(properties).
+                                                  buildServiceRegistry();
+        SessionFactory sessionFactory = configuration.
+                                           buildSessionFactory(serviceRegistry);
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            Sport sport = new Sport(1, "Soccer");
+            session.save(sport);
+            session.flush();
+            tx.commit();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            tx.rollback();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+        System.exit(0);
     }
 }
