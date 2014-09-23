721c48fc3fb9b8bbf64676473141851919f5d20b
Curtis Wilson <wilson.1339@osu.edu>
2014-09-23 16:10:09 -0400

    - Added unit test for toDetailedString. - Added TODO comments.

--- src/test/java/net/shnee/bitys/model/SportTest.java
+++ src/test/java/net/shnee/bitys/model/SportTest.java
@@ -13,15 +13,20 @@ import static org.junit.Assert.*;
  * Unit test for the class Sport
  */
 public class SportTest {
-    
+
     private SessionFactory sessionFactory;
     private Session session = null;
-    
+
     public SportTest() {
     }
 
     @Before
     public void setUp() {
+
+        /* TODO create class for this code, it will be used in all model unit
+         * tests. */
+        // TODO This code needs to be moved to a unit test for dao
+
         // setup the session factory
         AnnotationConfiguration configuration = new AnnotationConfiguration();
         configuration.addAnnotatedClass(Sport.class);
@@ -35,7 +40,7 @@ public class SportTest {
         this.sessionFactory = configuration.buildSessionFactory();
         this.session = sessionFactory.openSession();
     }
-    
+
     @After
     public void tearDown() {
         this.session.close();
@@ -46,15 +51,29 @@ public class SportTest {
      * Test of toString method, of class Sport.
      */
     @Test
+    public void testToStringDeatiled() {
+        LoggerFactory.getLogger("model." + SportTest.class.getSimpleName()).
+                      debug("testToStringDetailed");
+        Sport sport = new Sport(1, "Football");
+        String expResult = "Sport: id=1, name=Football";
+        String result = sport.toStringDetailed();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of toString method, of class Sport.
+     */
+    @Test
     public void testToString() {
         LoggerFactory.getLogger("model." + SportTest.class.getSimpleName()).
                       debug("testToString");
         Sport sport = new Sport(1, "Football");
-        String expResult = "Sport: id=1, name=Football";
+        String expResult = "Football";
         String result = sport.toString();
         assertEquals(expResult, result);
     }
 
+    // TODO Add test with null value for name.
     /**
      * Test of getName method, of class Sport.
      */
@@ -63,10 +82,10 @@ public class SportTest {
         LoggerFactory.getLogger("model." + SportTest.class.getSimpleName()).
                       debug("testGetName");
         Sport sport = new Sport();
-       
-        // Default value shoud be the empty string.    
+
+        // Default value shoud be the empty string.
         assertEquals("", sport.getName());
-        
+
         sport.setName("New Name");
         assertEquals("New Name", sport.getName());
     }
