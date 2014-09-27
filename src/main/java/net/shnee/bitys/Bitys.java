99868c296c55814b94bca146fb49867a247671d5
Curtis Wilson <wilson.1339@osu.edu>
2014-09-26 23:41:25 -0400

    - Currently just populates the Sports table.

--- src/main/java/net/shnee/bitys/Bitys.java
+++ src/main/java/net/shnee/bitys/Bitys.java
@@ -1,14 +1,11 @@
 package net.shnee.bitys;
 
-import java.util.Properties;
+import java.util.List;
 import net.shnee.bitys.db.Db;
+import net.shnee.bitys.db.populate.PopulateSport;
 import net.shnee.bitys.model.Sport;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,28 +19,9 @@ public class Bitys
     {
         Logger log = LoggerFactory.getLogger("Bitys");
         log.info("Word to your mother...");
+            
+        PopulateSport.deleteAllAndPopulate();
         
-        Db db = Db.getInstance();
-        
-        Session session = null;
-        Transaction tx = null;
-        try {
-            session = db.createSession();
-            Sport sprt = new Sport(1, "Testing");
-            session.save(sprt);
-            tx = session.beginTransaction();
-            Sport sport = new Sport(1, "Soccer");
-            session.save(sport);
-            session.flush();
-            tx.commit();
-        } catch(Exception ex) {
-            ex.printStackTrace();
-            tx.rollback();
-        } finally {
-            if(session != null) {
-                session.close();
-            }
-        }
         System.exit(0);
     }
 }
