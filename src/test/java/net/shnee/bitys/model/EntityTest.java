6eca2bb7b027b607c3edb1433c4195ebd818c129
Curtis Wilson <wilson.1339@osu.edu>
2014-09-29 14:27:31 -0400

    - Added test for getAll that will test any class derived from Entity.

--- src/test/java/net/shnee/bitys/model/EntityTest.java
+++ src/test/java/net/shnee/bitys/model/EntityTest.java
@@ -1,5 +1,11 @@
 package net.shnee.bitys.model;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,11 +17,19 @@ import static org.junit.Assert.*;
 // TODO Implement EntityTest
 
 /**
- * 
+ *
  */
+@RunWith(Parameterized.class)
 public class EntityTest {
-    
-    public EntityTest() {
+
+    private Entity entity;
+    private Class<?> clazz;
+    private Constructor<?> ctor;
+
+    public EntityTest(Entity entity) throws NoSuchMethodException {
+        this.entity = entity;
+        this.clazz = entity.getClass();
+        this.ctor = clazz.getConstructor();
     }
 
     @BeforeClass
@@ -25,11 +39,11 @@ public class EntityTest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -38,10 +52,22 @@ public class EntityTest {
      * Test of getAll method, of class Entity.
      */
     @Test
-    public void testGetAll() {
-        List<Sport> sports = Entity.getAll(Sport.class);
-        System.out.println("Output: " + sports);
+    public void testGetAll() throws InstantiationException,
+                                    IllegalAccessException,
+                                    IllegalArgumentException,
+                                    InvocationTargetException {
+        Collection entities = Arrays.asList(
+                this.ctor.newInstance(),
+                this.ctor.newInstance(),
+                this.ctor.newInstance()
+                );
+
+        Entity.saveOrUpdateAll(entities);
+        List entitiesRetrieved = Entity.getAll(this.entity.getClass());
+        assertEquals(entities, entitiesRetrieved);
     }
+
+    // TODO See if you can use reflection to run Sports.getAll()
 
     /**
      * Test of getId method, of class Entity.
@@ -52,9 +78,9 @@ public class EntityTest {
         Entity instance = new EntityImpl();
         Integer expResult = null;
         Integer result = instance.getId();
-        assertEquals(expResult, result);
+        //assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
     /**
@@ -67,23 +93,15 @@ public class EntityTest {
         Entity instance = new EntityImpl();
         instance.setId(id);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPluralName method, of class Entity.
-     */
-    @Test
-    public void testGetPluralName() {
-        System.out.println("getPluralName");
-        Entity instance = new EntityImpl();
-        String expResult = "";
-        String result = instance.getPluralName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
     public class EntityImpl extends Entity {
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> instancesToTest() {
+        Object[][] data = new Object[][] { {new Sport()} };
+        return Arrays.asList(data);
     }
 }
